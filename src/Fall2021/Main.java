5d57b3f1b89c603c52ff76b155d2165818d999c6
sevinco <95380565+sevinco@users.noreply.github.com>
2022-02-14 11:51:14 -0500

    Update Main.java

--- src/Fall2021/Main.java
+++ src/Fall2021/Main.java
@@ -4,12 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("1st class");
-
         System.out.println("2nd step");
-
         System.out.println("third step");
         System.out.println("forth class");
-        System.out.println("fifth class");
-        System.out.println("sixth clas");
+        
     }
 }
