1cec7a6cd174243fb802904a5071d957d5316ffd
sevinco <ozmensevinc@gmail.com>
2022-02-14 11:53:46 -0500

    My new class

--- src/Fall2021/Main.java
+++ src/Fall2021/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("1st class");
-        System.out.println("2nd step");
-        System.out.println("third step");
-        System.out.println("forth class");
-        
+
+        System.out.println("step");
+
+        System.out.println("step");
+        System.out.println("class");
+        System.out.println("class");
+        System.out.println("clas");
     }
 }
