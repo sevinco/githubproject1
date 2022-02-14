d9188c03cb8cc17ca05f3f8f448126940619d9a4
sevinco <ozmensevinc@gmail.com>
2022-02-14 11:21:26 -0500

    My new class

--- src/Fall2021/Second_class.java
+++ src/Fall2021/Second_class.java
@@ -4,6 +4,7 @@ public class Second_class {
     public static void main(String[] args) {
         System.out.println("new page second class");
         System.out.println("how are you");
+        System.out.println("everything is fine");
     }
 
 }
