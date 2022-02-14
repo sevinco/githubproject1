ece41949a7157e09f7c724a2004f1f801e20564f
sevinco <ozmensevinc@gmail.com>
2022-02-14 11:14:06 -0500

    My new class

--- src/Fall2021/Second_class.java
+++ src/Fall2021/Second_class.java
@@ -3,6 +3,7 @@ package Fall2021;
 public class Second_class {
     public static void main(String[] args) {
         System.out.println("new page second class");
+        System.out.println("how are you");
     }
 
 }
